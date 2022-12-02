569f637ab52310b9733cb2ec1fe5ae5380b5e5a4
trent earl <t@kilyos.xyz>
2022-12-02 12:58:31 +0300

    02 part 2

--- 02.py
+++ 02.py
@@ -1,26 +1,32 @@
 
 ITEM_SCORE = { 'rock': 1, 'paper': 2, 'scissors': 3 }
 ROUND_SCORE = { 'loss': 0, 'draw': 3, 'win': 6 }
-ITEM = { 'A': 'rock', 'B': 'paper', 'C': 'scissors', 'X': 'rock', 'Y': 'paper', 'Z': 'scissors' }
+ITEM = { 'A': 'rock', 'B': 'paper', 'C': 'scissors' }
+OUTCOME = { 'X': 'loss', 'Y': 'draw', 'Z': 'win' }
 WINNERS = { 'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock' }
+LOSERS = { 'rock': 'paper', 'scissors': 'rock', 'paper': 'scissors' }
 
 lines = [ l.strip() for l in open('./02.data', 'r').readlines() ]
 rounds = []
 
 for line in lines:
-    rounds.append(list(map(lambda k: ITEM[k], line.split(' '))))
+    a, b = line.split(' ')
+    rounds.append((ITEM[a], OUTCOME[b]))
 
-score = []
+scores = []
 for r in rounds:
-    them, me = r[0], r[1]
-    if WINNERS[them] == me:
-        status = 'loss'
-    elif them == me:
-        status = 'draw'
+    them, outcome = r[0], r[1]
+    score = ROUND_SCORE[outcome]
+
+    if outcome == 'loss':
+        item = WINNERS[them]
+    elif outcome == 'draw':
+        item = them
     else:
-        status = 'win'
-    score.append(ROUND_SCORE[status] + ITEM_SCORE[me])
+        item = LOSERS[them]
+
+    scores.append(score + ITEM_SCORE[item])
 
 
-print(sum(score))
+print(sum(scores))
 
